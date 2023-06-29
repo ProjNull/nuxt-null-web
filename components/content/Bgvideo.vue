8fbe78c5fe0f49816c0986673b5877c0ca6bf62f
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2023-06-29 12:37:45 +0200

    Added assets

---
+++ components/content/Bgvideo.vue
@@ -0,0 +1,23 @@
+<template>
+    <video class="video" autoplay loop muted src="/public/bg.mp4"/>
+</template>
+
+<style scoped>
+    .video {
+        height: 40rem;
+        object-fit: contain;
+        width: 100%;
+        margin-bottom: -5rem;
+        background-color: black;
+    }
+
+    @media screen and (max-width: 600px) {
+        .video {
+        height: 20rem;
+        object-fit: cover;
+        width: 100%;
+        margin-bottom: -2rem;
+        background-color: rgb(0, 0, 0);
+    }
+    }
+</style>
