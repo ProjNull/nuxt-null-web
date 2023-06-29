359ed14a1be4174540725e707f7dec797a0a9481
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2023-06-29 14:53:29 +0200

    Added cookies

--- app.vue
+++ app.vue
@@ -1,3 +1,27 @@
+<script setup>
+const {
+  cookiesEnabled,
+  cookiesEnabledIds,
+  isConsentGiven,
+  isModalActive,
+  moduleOptions
+} = useCookieControl()
+
+// example: react to a cookie being accepted
+watch(
+  () => cookiesEnabledIds.value,
+  (current, previous) => {
+    if (
+      (!previous?.includes('google-analytics') &&
+        current?.includes('google-analytics'))
+    ) {
+      // cookie with id `google-analytics` got added
+      window.location.reload() // placeholder for your custom change handler
+    }
+  },
+  { deep: true }
+)
+</script>
 <template>
   <div>
     <NavBar brand="{NULL}">
@@ -21,4 +45,5 @@
     import "~/assets/css/navbar.css"
     import "~/assets/css/textstyles.css"
     import "~/assets/css/sections.css"
+    import "~/assets/css/cookies.css"
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,34 @@ export default defineNuxtConfig({
   //   preset: 'service-worker'
   // },
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@hypernym/nuxt-anime'],
+  modules: ['@nuxt/content', '@hypernym/nuxt-anime', '@dargmuesli/nuxt-cookie-control', 'nuxt-lazy-load'],
+  cookieControl: {
+    domain: 'null.mobilex1122.eu',
+    cookies: {
+      necessary: [
+      ],
+      optional: [
+        {
+          description: {
+            en:  'Used for Testing (No data saved)'
+          },
+          id: 'test1', // if unset, `getCookieId(cookie)` returns the cookie's slugified name instead, which e.g. is used to fill the state's `enabledCookieIds` list
+          // use a short cookie id to save bandwidth!
+          name: {
+            en: 'Test Cookie 1' // you always have to specify a cookie name (in English)
+          }
+        },
+        {
+          description: {
+            en:  'Used for Testing (No data saved)'
+          },
+          id: 'test2', // if unset, `getCookieId(cookie)` returns the cookie's slugified name instead, which e.g. is used to fill the state's `enabledCookieIds` list
+          // use a short cookie id to save bandwidth!
+          name: {
+            en: 'Test Cookie 2' // you always have to specify a cookie name (in English)
+          }
+        },
+      ],
+    }
+  }
 })
