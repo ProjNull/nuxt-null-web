bfb2a7f70324f0fae373bbbcd331cc4c860c1eb7
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2023-06-29 12:38:40 +0200

    Added anime.js framework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  //ssr: false,
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/content'
-  ],
+  modules: ['@nuxt/content', '@hypernym/nuxt-anime'],
 })
