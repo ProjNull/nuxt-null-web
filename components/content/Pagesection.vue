7f281e582e1ed88b12d766ae46025992f0b9af94
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2023-06-29 12:40:31 +0200

    Added section design

---
+++ components/content/Pagesection.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  type:String ("left")
+})
+</script>
+
+<template>
+    <div v-if="type == 'left' || type=='l'" class="section-left">
+      <div class="container-fit">
+        <slot/>
+      </div>
+    </div>
+    <div v-else-if="type == 'right' || type=='r'" class="section-right">
+      <div class="container-fit">
+        <slot/>
+      </div>
+    </div>
+    <div v-else class="section-none">
+      <div class="container-fit">
+        <slot/>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+  .container-fit {
+    max-width: 70rem;
+    margin-inline: auto;
+    padding-inline: 1rem;
+  }
+  
+</style>
